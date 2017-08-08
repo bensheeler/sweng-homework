f8eeffe91e6819112de42a699a0b7e98cccea681
Benjamin Sheeler <Benjamin_Sheeler@compaid.com>
2017-08-08 15:35:50 -0400

    adds logic for calculator

--- SwEngHomework/SwEngHomework/DescriptiveStatistics/Stats.cs
+++ SwEngHomework/SwEngHomework/DescriptiveStatistics/Stats.cs
@@ -1,9 +1,38 @@
-﻿namespace SwEngHomework.DescriptiveStatistics
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SwEngHomework.DescriptiveStatistics
 {
     public class Stats
     {
         public double Average { get; set; }
         public double Median { get; set; }
         public double Range { get; set; }
+
+        public Stats() { }
+
+        public Stats(IEnumerable<double> dataSet)
+        {
+            Average = Math.Round(dataSet.Average(), 2, MidpointRounding.AwayFromZero);
+            Median = Math.Round(CalculateMedian(dataSet), 2, MidpointRounding.AwayFromZero);
+            Range = Math.Round(dataSet.Max() - dataSet.Min(), 2, MidpointRounding.AwayFromZero);
+        }
+
+        private double CalculateMedian(IEnumerable<double> dataSet)
+        {
+            var orderedDataSet = dataSet.OrderBy(i => i);
+            var count = orderedDataSet.Count();
+            var halfIndex = count / 2;
+
+            if (orderedDataSet.Count() % 2 == 0)
+            {
+                return (orderedDataSet.ElementAt(halfIndex) 
+                    + orderedDataSet.ElementAt(halfIndex - 1)) 
+                    / 2;
+            }
+
+            return orderedDataSet.ElementAt(halfIndex);
+        }
     }
 }
--- SwEngHomework/SwEngHomework/DescriptiveStatistics/StatsCalculator.cs
+++ SwEngHomework/SwEngHomework/DescriptiveStatistics/StatsCalculator.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace SwEngHomework.DescriptiveStatistics
 {
@@ -6,8 +9,31 @@ namespace SwEngHomework.DescriptiveStatistics
     {
         public Stats Calculate(string semicolonDelimitedContributions)
         {
-            // TODO: your implementation
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(semicolonDelimitedContributions))
+                return new Stats();
+
+            var cleanInput = new StringBuilder(semicolonDelimitedContributions)
+                .Replace("$", string.Empty)
+                .Replace(" ", string.Empty)
+                .ToString();
+
+            var contributions = cleanInput.Split(';');
+
+            if (contributions.Length < 1)
+                return new Stats();
+
+            var decimalContributions = new List<double>();
+
+            foreach(var contribution in contributions)
+            {
+                if (double.TryParse(contribution, out double result))
+                    decimalContributions.Add(result);
+            }
+
+            if (decimalContributions.Count() < 1)
+                return new Stats();
+
+            return new Stats(decimalContributions);
         }
     }
 }
